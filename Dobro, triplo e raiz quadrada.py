b2ec8c70f44fbf2b45e391d5974c9d74955d9316
Erick Gonçalves <120678046+Saimonexk@users.noreply.github.com>
2023-01-04 17:04:15 -0300

    Add files via upload

---
+++ Dobro, triplo e raiz quadrada.py
@@ -0,0 +1,4 @@
+# Calculando o dobro, triplo e raiz quadrada de um número
+n = int(input('digite um número: '))
+print('O dobro de {} vale {}'.format(n, (n * 2)))
+print('o tripo de {} é {} \n raiz quadrada de {} é {:.0f} '.format(n, (n * 3), n, (n ** 1/2)))
